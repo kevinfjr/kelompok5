6c11415278b7a791100ad6af56cdbff1a3ed5c6e
kevinfjr <kevinfajarrp@gmail.com>
2025-06-24 17:39:56 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Terima kasih! Pesan Anda telah dikirim.");
-  this.reset();
-});
